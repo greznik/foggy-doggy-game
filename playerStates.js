254248dbc8a5cbd3fcd773919827fd0a917409a5
George Reznik <nereznik@gmail.com>
2022-08-27 11:14:30 +0500

    add paralax background

--- playerStates.js
+++ playerStates.js
@@ -23,7 +23,7 @@ export class Sitting extends State {
   }
   handleInput(input) {
     if (input.includes('ArrowLeft') || input.includes('ArrowRight'))
-      this.player.setState(states.RUNNING)
+      this.player.setState(states.RUNNING, 1)
   }
 }
 
@@ -38,8 +38,8 @@ export class Running extends State {
     this.player.frameY = 3
   }
   handleInput(input) {
-    if (input.includes('ArrowDown')) this.player.setState(states.SITTING)
-    else if (input.includes('ArrowUp')) this.player.setState(states.JUMPING)
+    if (input.includes('ArrowDown')) this.player.setState(states.SITTING, 0)
+    else if (input.includes('ArrowUp')) this.player.setState(states.JUMPING, 1)
   }
 }
 
@@ -56,7 +56,7 @@ export class Jumping extends State {
   }
   handleInput(input) {
     if (this.player.vy > this.player.weight)
-      this.player.setState(states.FALLING)
+      this.player.setState(states.FALLING, 1)
   }
 }
 
@@ -71,6 +71,6 @@ export class Falling extends State {
     this.player.frameY = 2
   }
   handleInput(input) {
-    if (this.player.onGround()) this.player.setState(states.RUNNING)
+    if (this.player.onGround()) this.player.setState(states.RUNNING, 1)
   }
 }
